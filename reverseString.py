6660a319fb59744e78caf33101465163c4693f52
samanth2975 <99540713+samanth2975@users.noreply.github.com>
2022-06-21 22:07:59 +0530

    Delete reverseString.py

--- reverseString.py
+++
@@ -1,9 +0,0 @@
-n = input('enter a string')
-print('entered string',n)
-    
-#print(n[::-1]) # using slice method [start:end:step]
-#another logic using for loop
-reverse =""
-for i in n:
-    reverse = i+reverse
-print(reverse)
